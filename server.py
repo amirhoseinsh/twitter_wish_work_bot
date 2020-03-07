b9bf7279b03ed602c525a9a7de9604b6e8377a0f
amirhosein <amirhhh.shirani@yahoo.com>
2020-03-07 13:46:47 +0330

    server.py revise for flask

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
-from os import environ
 from flask import Flask
+# from os import environ
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=environ.get('PORT'))
+
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+# app.run(host='0.0.0.0', port=environ.get('PORT'))
