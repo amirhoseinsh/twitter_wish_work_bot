2f6093478781c5b1341404febc1d0ad19de3044a
amirhosein <amirhhh.shirani@yahoo.com>
2020-03-07 14:22:00 +0330

    server.py revise for flask

--- server.py
+++ server.py
@@ -3,13 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, debug=True)
-
 
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, debug=True)
 
 
 # app.run(host='0.0.0.0', port=environ.get('PORT'))
